e5e34dd2e311beb96b2f839edaabbf8a943b1f0b
Gyutsby <nikeinkyu@naver.com>
2021-12-15 13:09:55 +0900

    1
    1

---
+++ src/test/java/com/example/projectspringshop/ProjectspringShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectspringshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectspringShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
